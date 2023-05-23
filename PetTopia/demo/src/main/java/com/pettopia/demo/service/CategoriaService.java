4be04a499e188b49cad2d3b7531ccbcd7b050483
Marcos Godinho <104370082+MarcosGodinho@users.noreply.github.com>
2023-05-22 22:09:13 -0300

    Add files via upload

---
+++ PetTopia/demo/src/main/java/com/pettopia/demo/service/CategoriaService.java
@@ -0,0 +1,12 @@
+package com.pettopia.demo.service;
+
+import java.util.List;
+
+import com.pettopia.demo.entity.Categoria;
+
+public interface CategoriaService {
+    List<Categoria> getAll();
+
+    void save(Categoria categoria);
+    void delete(long id);
+}
